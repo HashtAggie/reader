463b34ec7d6f7b7d159d1421b2ebf950b229448c
noraeb <n.blomaard@gmail.com>
2016-04-09 11:49:59 +0200

    Added weeks to databse

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Week.create( {name: "Week 1"})
+Week.create( {name: "Week 2"})
+Week.create( {name: "Week 3"})
+Week.create( {name: "Week 4"})
+Week.create( {name: "Week 5"})
